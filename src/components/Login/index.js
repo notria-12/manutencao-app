05f4dfa5e73646b7c2c9814ac250fd0ec4bc51fc
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-04-06 11:56:25 -0300

    login page finalized

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,30 +1,44 @@
 import './styles.css';
 
-function Login(){
+function Login() {
     return (
         <div>
             <header>
-                <nav className="navbar">
-                    <h3>SINC - MANUTENÇÃO</h3>
+                <nav className="navbar bg-primary ">
+                    <h3 className="text-white">SINC - MANUTENÇÃO</h3>
                 </nav>
             </header>
 
             <body>
                 <main className="container">
-                    <div className="form-login">
+                    <div className="card col-6 p-3">
+                        <div>
+                            <h4 className="text-center">Bem vindo de volta</h4>
+                            <p className="text-center">Entre com suas credenciais de acesso</p>
+                        </div>
                         <form>
-                            <label>Usuario</label>
-                            <input type="text"></input>
-                            <label>Senha</label>
-                            <input type="password"></input>
+                            <div className="form-floating">
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label for="floatingInput">Email</label>
+                            </div>
+                            <div className="form-floating mt-1">
+                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                <label for="floatingPassword">Senha</label>
+                            </div>
+
+
+                            <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">Entrar</button>
+
                         </form>
+
                     </div>
+
                 </main>
             </body>
-
+            {/* 
             <footer>
                 <p>Aqui meu rodapé</p>
-            </footer>
+            </footer> */}
         </div>
     );
 
