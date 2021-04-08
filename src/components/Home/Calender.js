c68d790180c75100bc41dfe5042f8cf0db8189df
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-04-08 16:56:23 -0300

    calendar schedule for activities

---
+++ src/components/Home/Calender.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import React from "react"
+
+function Calender(){
+    return(
+        <div id="container">
+
+        </div>
+    )
+}
+
+export default Calender;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,43 @@
-import React from "react";
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { INITIAL_EVENTS, createEventId } from '../../event-utils'
+import ptLocale from '@fullcalendar/core/locales/pt-br'
+import './styles.css'
 
-
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
             <header>
-
+                <nav className="navbar bg-primary text-white">
+                    <h3>
+                     Home
+                    </h3>
+                </nav>
             </header>
             <body>
-                <h1>Bem vindo ao APP MANUTENÇÃO</h1>
+                <div className="demo-app">
+                    <div className="demo-app-main">
+                    <FullCalendar
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        headerToolbar={{
+                          left: 'prev,next today',
+                          center: 'title',
+                          right: 'dayGridMonth,timeGridWeek',
+                          
+                        }}
+                        locales={[ptLocale]}
+                        initialView='dayGridMonth'
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        initialEvents={INITIAL_EVENTS}
+                        ></FullCalendar>
+                    </div>
+                </div>
+             
             </body>
         </div>
     );
